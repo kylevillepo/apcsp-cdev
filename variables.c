ea3d601236d9e603da6f107ad84b5448830f9b44
kylevillepo <villeponteauk1553@my.sduhsd.net>
2021-01-07 20:57:28 +0000

    adding variables.c

---
+++ variables.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+  int a;
+  int b = 3;
+  int c;
+
+  a = 2;
+  c = a + b;
+  printf("Sum of %d and %d is %d\n", a, b, c);
+
+  printf("a= %d, c = %d\n", c, a);
+}
