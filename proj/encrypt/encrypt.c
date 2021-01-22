37b8ee1f26200a8b64c35227281a4c4a091163bb
kylevillepo <villeponteauk1553@my.sduhsd.net>
2021-01-22 07:46:18 +0000

    final project, everything works but encrypt with save

--- proj/encrypt/encrypt.c
+++ proj/encrypt/encrypt.c
@@ -22,6 +22,41 @@ char shiftChar(char c, int shift, int direction)
   //   shiftChar('c', 3, 1) : 'f'
   //   shiftChar('S', 2, 0) : 'P'
   //   shiftChar('b', 3, 0) : '8'
+  int monke = 62;
+  int index = 0;
+  if (direction == 1){
+    for (int i = 0; i < 62; i++){
+      if (CHARS[i] == c){
+        index = i + shift;
+        if (index > 62)
+          index = index - 62;
+        if (index < 0)
+          index = index + 62;
+        return CHARS[index];
+      } else {
+        monke = monke - 1;
+      }
+    }
+  if (monke == 0){
+    return c;
+  }
+  } else if (direction == 0){
+    for (int i = 0; i < 62; i++){
+      if (CHARS[i] == c){
+        index = i - shift;
+        if (index > 62)
+          index = index - 62;
+        if (index < 0)
+          index = index + 62;
+        return CHARS[index];
+      } else {
+        monke = monke - 1;
+      }
+    }
+  if (monke == 0){
+    return c;
+  }
+  }
 }
 
 
--- proj/student.c
+++ proj/student.c
@@ -25,12 +25,25 @@ void createStudent(char* fname, char* lname, int age, int id)
   // student to the student array
   //  - the firstName and lastName strings should be dynamically created
   //    based on the size of the fname and lname args
+  Student* st = (Student*)malloc(sizeof(Student));
+
+  st->firstName = (char*)malloc((strlen(fname) + 1) * sizeof(char));
+  st->lastName = (char*)malloc((strlen(lname) + 1) * sizeof(char));
+  strcpy(st->firstName,fname);
+  strcpy(st->lastName,lname);
+  st->age = age;
+  st->id = id;
+  students[numStudents] = st;
+  numStudents++;
 }
 
 
-void deleteStudent(Student* student)
+void deleteStudent(Student* st)
 {
   // free the memory associated with a student including the strings
+  free(st->firstName);
+  free(st->lastName);
+  free(st);
 }
 
 
@@ -38,6 +51,11 @@ void deleteStudents()
 {
   // iterate over the students array deleting every student and setting te pointer
   // values to 0 and adjusting the numStudents to 0
+  for (int i = 0; i < numStudents; i++){
+    deleteStudent(students[i]);
+    students[i] = 0;
+  }
+  numStudents = 0;
 }
 
 
@@ -48,13 +66,69 @@ void saveStudents(int key)
   //   - the format of the file is one line per student as follows fname lname age id:
   //       tom thumb 15 1234 
   //       james dean 21 2345 
-  //       katy jones 18 4532 
+  //       katy jones 18 4532
+  // the best way to do this is to convert the student data to a string using sprintf and then
+  // (optionally) encrypt the whole string before writing it to disk (see cdemo/fileio3.c)
+  FILE* fp;
+
+  fp = fopen("studentdata.txt", "w");
+  if (fp) {
+  char buff[256];
+  for (int i = 0; i < numStudents; i++){
+    Student* st = students[i];
+    sprintf(buff,"%s %s %d %ld",st->firstName,st->lastName,st->age,st->id);
+    if (key != 0){
+      caesarEncrypt(buff, key);
+    }
+    printf("saving: %s\n",buff);
+    fprintf(fp,"%s\n",buff);
+  }
+  }
+  printf("saved %d students",numStudents);
+  fclose(fp);
 }
 
 
 void loadStudents(int key)
 {
   // load the students from the data file overwriting all exisiting students in memory
+  //  - make sure you first deleteStudents before loading new ones
+  //  - when loading it is best to load data into four strings and then (optionally) decrypt the
+  //    strings
+  //  - call createStudent to correctly create the students
+  if (numStudents > 0){
+    deleteStudents();
+  }
+  FILE* fp;
+
+  fp = fopen("studentdata.txt", "r");
+  while(1){
+    if (fp) {
+      char fnamebuff[256];
+      char lnamebuff[256];
+      char ageS[256];
+      char idS[256];
+      long id;
+      int age;
+      int match = fscanf(fp,"%s %s %s %s", fnamebuff, lnamebuff, ageS, idS);
+      if (match == 4){
+        if (key != 0){
+          caesarDecrypt(fnamebuff,key);
+          caesarDecrypt(lnamebuff,key);
+          caesarDecrypt(ageS,key);
+          caesarDecrypt(idS,key);
+        }
+      sscanf(ageS,"%d",&age);
+      sscanf(idS,"%ld",&id);
+      createStudent(fnamebuff, lnamebuff, age, id);
+      } else {
+        break;
+      }
+    }
+  }
+    printf("loaded %d students",numStudents);
+
+  fclose(fp);
 }
 
 
